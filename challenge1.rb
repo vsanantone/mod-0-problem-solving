509a2aeb0e036f2a8a7118a908ed18022f3bd3a2
Victor Sanchez <vsanantone@gmail.com>
2023-04-14 14:49:20 -0500

    Add challenge1.rb file

--- challenge1.rb
+++ challenge1.rb
@@ -6,10 +6,39 @@
 #We call the variable and print out those strings matching the requirement.
 
 
-people_or_things = ["dinosaur", "Dan", "Parker", "Edwin", "Derek", "Paul", "Mo", "Two", "teddy bear"]
+# people_or_things = ["dinosaur", "Dan", "Parker", "Edwin", "Derek", "Paul", "Mo", "Two", "teddy bear"]
 
-people_or_things.each do |person_or_thing|
-  if person_or_thing.length == 4
-      p person_or_thing
-  end
+# people_or_things.each do |person_or_thing|
+#   if person_or_thing.length == 4
+#       p person_or_thing
+#   end
+# end
+
+# 1. Start with an array of strings with a mix of uppercase and lowercase letters. Print every word in all lowercase letters.
+
+squares = ["rUbics cube", "ICE cUbe", "block", "bOX"]
+
+
+squares.each do |square|
+  lower_case_square = square.downcase
+      p lower_case_square
 end
+
+
+
+# 1. Write a method or function that accepts an array of strings as an argument. The method or function should return an array of only the words that include the letter combination "ing".
+
+
+words = ["running", "fish", "flying"]
+
+def return_ing(array)
+  ing_words = []
+  array.each do |word|
+    if word.include?("ing")
+      ing_words.push(word)
+    end
+  end
+  ing_words
+end
+
+print return_ing(words)

abd665e65edd3563d3c1bc5d6a9202f0287a68f7
Victor Sanchez <vsanantone@gmail.com>
2023-04-14 17:10:26 -0500

    Add challenge1.rb file

--- challenge1.rb
+++ challenge1.rb
@@ -6,15 +6,20 @@
 #We call the variable and print out those strings matching the requirement.
 
 
-# people_or_things = ["dinosaur", "Dan", "Parker", "Edwin", "Derek", "Paul", "Mo", "Two", "teddy bear"]
+people_or_things = ["dinosaur", "Dan", "Parker", "Edwin", "Derek", "Paul", "Mo", "Two", "teddy bear"]
 
-# people_or_things.each do |person_or_thing|
-#   if person_or_thing.length == 4
-#       p person_or_thing
-#   end
-# end
+people_or_things.each do |person_or_thing|
+  if person_or_thing.length == 4
+      p person_or_thing
+  end
+end
 
 # 1. Start with an array of strings with a mix of uppercase and lowercase letters. Print every word in all lowercase letters.
+
+#Create an array with strings with a plural name. I am going to make it out of squares or square related objects
+#We are going to perform iteration with the each method to iterate through the array
+#We are going to create a variable that is assigned an element attached with the downcase method.
+#We are going to have an output of the variable in the iteration with "p"
 
 squares = ["rUbics cube", "ICE cUbe", "block", "bOX"]
 
@@ -27,11 +32,16 @@ end
 
 
 # 1. Write a method or function that accepts an array of strings as an argument. The method or function should return an array of only the words that include the letter combination "ing".
+#We are creating a method with the def, method name, and end key words. We want to create a parameter that accepts an array
+#We want to search for element strings that have the chr = "ing"
+# We want to create a variable assigned to an empty array so we can push the elements we find into a new array.
+#With a conditional, if word includes "ing", we are going to push into the new array.
+#We want to return the value of the new array and call it
+#We want to print the method, pass the array into as an argument and see the new array print to the console.
 
+drivers = ["braking", "car", "driving", "starting", "speed"]
 
-words = ["running", "fish", "flying"]
-
-def return_ing(array)
+def search_ing(array)
   ing_words = []
   array.each do |word|
     if word.include?("ing")
@@ -41,4 +51,4 @@ def return_ing(array)
   ing_words
 end
 
-print return_ing(words)
+print search_ing(drivers)
